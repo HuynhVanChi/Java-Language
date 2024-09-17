5920ee41023fb8a6eb4fce70b4c792ee04279ccb
HuynhVanChi <huynhvanchi978@gmail.com>
2024-09-17 13:56:17 +0700

    updata

---
+++ THUC_HANH_TREN_LOP/Buoi1/vehinh3.java
@@ -0,0 +1,14 @@
+public class vehinh3{
+    public static void drawBox()
+    {
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+    }
+    public static void main(String[] args) {
+        drawBox();
+        System.out.println("");
+        drawBox();
+    }
+}
---
+++ THUC_HANH_TREN_LOP/Buoi1/vehinh6.java
@@ -0,0 +1,47 @@
+public class vehinh6 {
+    
+    public static void main(String[] args) {
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+        System.out.println();
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println();
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println("+------+ ");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("|  Viet|");
+        System.out.println("|   Nam|");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+       // Thuc hanh 5
+        System.out.println();
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println();
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        
+    }
+}
