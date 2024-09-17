0172aa69d006b5bbe92114c68fa1fc655c3b37ce
HuynhVanChi <huynhvanchi978@gmail.com>
2024-09-17 13:35:06 +0700

    updata

---
+++ THUC_HANH_TREN_LOP/Buoi1/Buoi1/Bai1.java
@@ -0,0 +1,15 @@
+public class Bai1 {
+    public static void main(String[] args) {
+      System.out.println("Hello Word");
+      // \t tap
+      System.out.println("Chao mung sinh vien nam 2 \t Hocj phan Java");
+      // \n qua dong moi
+      System.out.println("Khoa ky thuat Cong nghe \nTruong dai hoc HUng Vuong TpHCM");
+      // \" dau ngoac kep 
+      System.out.println("\"Trung nghia - Tu tin - Trach nhiem\"");
+      // \\ dau ngoac cheo nguoc 
+      System.out.println("Chung toi la sinh vien \\ Khoa 2023");
+    }
+  }
+  
+  
